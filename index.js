7cb5eebc7b639fd0dd49ec7b4b701c82d7294505
Rajeevkumar69 <rk488296@gmail.com>
2025-08-20 18:04:41 +0530

    Added helmet

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from 'mongoose';
+import helmet from "helmet";
 import router from "./routes/product-route.js";
 
 dotenv.config();
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(helmet());
 
 const dbUrl = process.env.DATABASE_URL; 
 const dbName = 'product'; 
